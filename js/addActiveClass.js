dce2c0cbe5b1bfb5de296dd0c10a52dc3d65524d
katya-sy <katya_sy_28@mail.ru>
2023-10-17 21:09:34 +0900

    add active for links nav

--- js/addActiveClass.js
+++ js/addActiveClass.js
@@ -1,10 +1,21 @@
-let openMenu = document.getElementById("burger");
-let sidebar = document.getElementById("sidebar");
+const openMenu = document.getElementById("burger");
+const sidebar = document.getElementById("sidebar");
 openMenu.addEventListener("click", () => {
   sidebar.classList.add("sidebar--active");
 });
 
-let closeMenu = document.getElementById("close-menu");
+const closeMenu = document.getElementById("close-menu");
 closeMenu.addEventListener("click", () => {
   sidebar.classList.remove("sidebar--active");
 });
+
+const linksNav = document.querySelectorAll(".sidebar__nav-item");
+console.log(linksNav);
+for (let index = 0; index < linksNav.length; index++) {
+  linksNav[index].addEventListener("click", () => {
+    for (let index = 0; index < linksNav.length; index++) {
+      linksNav[index].classList.remove("active");
+    }
+    linksNav[index].classList.add("active");
+  });
+}
