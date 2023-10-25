6bb725ac1a258b9616f0c428fb084ad29396053e
katya-sy <katya_sy_28@mail.ru>
2023-10-25 14:41:17 +0900

    style pagination bullets, destroy swiper on breakpoints

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
+const breakpoint = window.matchMedia("(min-width:768px)");
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+let swiper;
+
+const breakpointChecker = function () {
+  if (breakpoint.matches === true) {
+    if (swiper !== undefined) swiper.destroy(true, true);
+    return;
+  } else {
+    return enableSwiper();
+  }
+};
+
+const enableSwiper = function () {
+  swiper = new Swiper(".swiper", {
+    // Optional parameters
+    slidesPerView: 1.2,
+    direction: "horizontal",
+    spaceBetween: 16,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+};
+
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
