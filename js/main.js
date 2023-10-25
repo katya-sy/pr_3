881879f0662bd2245c1e31354e6d0e14ff53982f
katya-sy <katya_sy_28@mail.ru>
2023-10-25 13:51:23 +0900

    add section brands (not ready), swiper

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
