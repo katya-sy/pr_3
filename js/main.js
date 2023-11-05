d0787d5f9a5ae01204e38df87f97dc0a7902ca5d
katya-sy <katya_sy_28@mail.ru>
2023-11-05 18:56:58 +0900

    edit swiper, add overlay

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+// sidebar
+const openMenuBtn = document.getElementById("burger");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.querySelector(".overlay");
+openMenuBtn.addEventListener("click", () => {
+  sidebar.classList.add("sidebar--active");
+  overlay.style.display = "block";
+});
+
+const closeMenuBtn = document.getElementById("close-menu");
+closeMenuBtn.addEventListener("click", () => {
+  sidebar.classList.remove("sidebar--active");
+  overlay.style.display = "none";
+});
+
+// const linksNav = document.querySelectorAll(".sidebar__nav-item");
+// console.log(linksNav);
+// for (let index = 0; index < linksNav.length; index++) {
+//   linksNav[index].addEventListener("click", () => {
+//     for (let index = 0; index < linksNav.length; index++) {
+//       linksNav[index].classList.remove("active");
+//     }
+//     linksNav[index].classList.add("active");
+//   });
+// }
+
+const nav = document.querySelector(".sidebar__nav");
+let activeEl = document.querySelector(".sidebar__nav-links--active");
+nav.addEventListener("click", (e) => {
+  if (activeEl === e.target) return;
+  if (e.target.classList.contains("sidebar__nav-links")) {
+    if (activeEl) activeEl.classList.remove("sidebar__nav-links--active");
+    activeEl = e.target;
+    e.target.classList.add("sidebar__nav-links--active");
+  }
+});
+
+// swiper
 const breakpoint = window.matchMedia("(min-width:768px)");
 
 let swiper;
